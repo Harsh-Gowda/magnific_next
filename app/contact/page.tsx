12bf408c7cadc2381cd334a5d37966d5ead44328
harsh <harshgowdaujire@gmail.com>
2026-09-08 20:04:46 +0530

    dffd

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -47,69 +47,69 @@ export default async function ContactPage() {
                 <div className='absolute inset-0 -z-10 bg-black/60 '></div>
 
 
-                    <div className=' relative z-10 pb-20'>
-                        <div className='flex justify-center items-center min-h-[400px]'>
+                <div className=' relative z-10 pb-20'>
+                    <div className='flex justify-center items-center min-h-[400px]'>
 
-                                 <h1 className='text-3xl font-bold text-white'>{data.title}</h1>
+                        <h1 className='text-3xl font-bold text-white'>{data.title}</h1>
+                    </div>
+                    <div className=' bg-white min-h-[350px] opacity-50  justify-center flex text-center items-center '>
+                        <p className='text-black'>{data.description}</p>
+                    </div>
+                    <div className='grid grid-cols-2 md:grid-cols-4  text-center '>
+                        <div className='bg-[#edeae3] p-10 '>
+                            <h1 className='text-2xl font-bold text-black'>Email</h1>
+                            <p className='text-black'>{data.email}</p>
                         </div>
-                        <div className=' bg-white min-h-[350px] opacity-50  justify-center flex text-center items-center '>
-                            <p className='text-black'>{data.description}</p>
+                        <div className='bg-[#625947] p-10'>
+                            <h1 className='text-2xl font-bold text-black'>Phone</h1>
+                            <p className='text-black'>{data.phone}</p>
                         </div>
-                        <div className='grid grid-cols-2 md:grid-cols-4  text-center '>
-                            <div className='bg-[#edeae3] p-10 '>
-                                <h1 className='text-2xl font-bold text-black'>Email</h1>
-                                <p className='text-black'>{data.email}</p>
-                            </div>
-                            <div className='bg-[#625947] p-10'>
-                                <h1 className='text-2xl font-bold text-black'>Phone</h1>
-                                <p className='text-black'>{data.phone}</p>
-                            </div>
-                            <div className='bg-[#edeae3] p-10'>
-                                <h1 className='text-2xl font-bold text-black'>Email</h1>
-                                <p className='text-black'>{data.email}</p>
-                            </div>
-                            <div className='bg-[#625947] p-10'>
-                                <h1 className='text-2xl font-bold text-black'>Address</h1>
-                                <p className='text-black'>{data.address}</p>
-                            </div>
-
-
-
+                        <div className='bg-[#edeae3] p-10'>
+                            <h1 className='text-2xl font-bold text-black'>Email</h1>
+                            <p className='text-black'>{data.email}</p>
                         </div>
-
-                        <div className='flex flex-row w-[100%]  justify-center text-center'>
-                            <div className='bg-[#969291] w-[50%] p-10'>
-                                <h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
-                            </div>
-                            <div className='bg-[#636260] w-[50%] p-10'>
-                                <h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
-
-                            </div>
+                        <div className='bg-[#625947] p-10'>
+                            <h1 className='text-2xl font-bold text-black'>Address</h1>
+                            <p className='text-black'>{data.address}</p>
                         </div>
 
 
-                         {/* Contact Us Design */}
-
-                         <div className='flex flex-col w-[100%]  justify-center text-center h-[500px]'>
-                            <div className=""> 
-                              < h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
-                            </div>
-                            <div className="flex flex-row w-[100%] justify-center text-center">
-                                <div className=" w-[50%] p-20"> 
-                                    <h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
-                                    </div>
-                                <div className=" w-[50%] p-20">
-                                     <h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
-                                     </div>
-                            </div>
-                         </div>
 
                     </div>
 
-                
+                    <div className='flex flex-row w-[100%]  justify-center text-center'>
+                        <div className='bg-[#969291] w-[50%] p-10'>
+                            <h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
+                        </div>
+                        <div className='bg-[#636260] w-[50%] p-10'>
+                            <h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
+
+                        </div>
+                    </div>
+
+
+                    {/* Contact Us Design */}
+
+                    <div className='flex flex-col w-[100%]  justify-center text-center h-[500px]'>
+                        <div className="">
+                            < h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
+                        </div>
+                        <div className="flex flex-row w-[100%] justify-center text-center">
+                            <div className=" w-[50%] p-20">
+                                <h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
+                            </div>
+                            <div className=" w-[50%] p-20">
+                                <h1 className='text-2xl font-bold text-white'>Magnific Design fans</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
             </section>
 
-           
+
 
 
 
