12ee9d450543ac60e10dee48d25ae0bf61c5b3ef
harsh <harshgowdaujire@gmail.com>
2026-09-10 15:29:01 +0530

    dfe

--- app/components/Location/Location.tsx
+++ app/components/Location/Location.tsx
@@ -2,64 +2,64 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 
-type Location ={
-    id:number;
-    name:string;
-    address:string;
-    longitude:number;
-    latitude:number;
-    phone:string;
-    email:string;
-    image:{
-        url:string;
-        alt:string;
+type Location = {
+    id: number;
+    name: string;
+    address: string;
+    longitude: number;
+    latitude: number;
+    phone: string;
+    email: string;
+    image: {
+        url: string;
+        alt: string;
     };
-    direction:string;
+    direction: string;
 }
 
-type LocationProps ={
-    locations:Location[];
+type LocationProps = {
+    locations: Location[];
 };
 
-export default function Location({locations} : LocationProps){
+export default function Location({ locations }: LocationProps) {
 
-    return(
-            <main>
-
-
-                <div className='flex flx-col gap-10 overflow-x-auto'>
-                    {
-                        locations.map((location)=>(
-                            <div key={location.id} className='min-w-[200px] text-black p-5 bg-amber-100'>
-                                    <h1>{location.name}</h1>
-
-                            </div>
-                        ))
-                    }
-
-                </div>
+    return (
+        <main>
 
 
-                <div>
+            <div className='flex flx-col gap-10 overflow-x-auto'>
                 {
-                    locations.map((location)=>(
-                        <div key={location.id} className=' '>
-                            
-                            <div className='flex flex-col'>
-                                <div className='flex flex-row'>
-                                    <h1 className='text-white'>{location.name}</h1>
-                                </div>                              
-                                <h1>{location.address}</h1>
-                                <h1>{location.phone}</h1>
-                                <h1>{location.email}</h1>
-                            </div> 
-                                
+                    locations.map((location) => (
+                        <div key={location.id} className='min-w-[200px] text-black p-5 bg-amber-100'>
+                            <h1>{location.name}</h1>
 
                         </div>
                     ))
                 }
-                </div>
-            </main>
+
+            </div>
+
+
+            <div>
+                {
+                    locations.map((location) => (
+                        <div key={location.id} className=' '>
+
+                            <div className='flex flex-col'>
+                                <div className='flex flex-row'>
+                                    <h1 className='text-white'>{location.name}</h1>
+                                </div>
+                                <h1>{location.address}</h1>
+                                <h1>{location.phone}</h1>
+                                <h1>{location.email}</h1>
+                            </div>
+
+
+                        </div>
+                    ))
+                }
+            </div>
+        </main>
     )
 
 }
